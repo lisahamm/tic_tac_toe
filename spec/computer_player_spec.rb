45e190016a0eb6c226f4b0eda5804d9e380e8144
Lisa Hamm <lisalhamm@gmail.com>
2014-08-21 18:44:17 -0500

    computer_player_spec change

--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -3,19 +3,13 @@ require "spec_helper"
 module TicTacToe
   describe ComputerPlayer do
     let(:computer_player) {ComputerPlayer.new('O')}
-    let(:board) {Board.new}
 
     describe "#take_turn" do
       it "makes a winning move when possible" do
-        board.set_cell(1, 'O')
-        expect(board.get_cell(1).value).to eq 'O'
-        board.set_cell(2, 'O')
-        board.set_cell(4, 'X')
-        board.set_cell(5, 'X')
+        board = Board.new([Cell.new('O'), Cell.new('O'), Cell.new, Cell.new('X'), Cell.new('X'), Cell.new, Cell.new, Cell.new, Cell.new])
         computer_player.take_turn(board)
         expect(board.get_cell(3).value).to eq 'O'
       end
     end
-
   end
 end
