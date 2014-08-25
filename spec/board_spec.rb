86ffab08b4acccaf304b71eb55d33f94706ad87a
Lisa Hamm <lisalhamm@gmail.com>
2014-08-24 21:09:06 -0500

    Refactored tests for board class

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -26,7 +26,7 @@ module TicTacToe
     end
 
     describe "#dup" do
-      it "can creates a new copy" do
+      it "creates a new copy" do
         board.set_cell(1, 'X')
         dup = board.dup
         expect(dup.get_cell(1).value).to eq 'X'
@@ -52,37 +52,53 @@ module TicTacToe
     end
 
     describe "#empty_cells" do
-      it "can return all empty cells" do
+      it "returns the empty cell numbers" do
         expect(board.empty_cells).to eq [1, 2, 3, 4, 5, 6, 7, 8, 9]
       end
     end
 
     describe "#get_winning_mark" do
-      it "can return the winner's mark" do
-        board.set_cell(1, 'X')
-        board.set_cell(2, 'X')
-        board.set_cell(3, 'X')
-        expect(board.get_winning_mark).to eq 'X'
+      context "when won" do
+        it "returns the winner's mark" do
+          expect(won_board.get_winning_mark).to eq 'X'
+        end
+      end
+
+      context "when no winner" do
+        it "is nil" do
+          expect(board.get_winning_mark).to eq nil
+        end
       end
     end
 
     describe "#winner?" do
-      it "can deterimine when a player has won the game" do
-        board.set_cell(1, 'X')
-        board.set_cell(2, 'X')
-        board.set_cell(3, 'X')
-        expect(board.winner?).to eq true
+      context "when won" do
+        it "is true" do
+          expect(won_board.winner?).to eq true
+        end
+      end
+
+      context "when tied" do
+        it "is false" do
+          expect(tie_board.winner?).to eq false
+        end
+      end
+
+      context "when all cells are empty" do
+        it "is false" do
+          expect(board.winner?).to eq false
+        end
       end
     end
 
     describe "#empty?" do
-      context "when entire board is empty" do
+      context "when all cells are empty" do
         it "is true" do
           expect(board.empty?).to eq true
         end
       end
 
-      context "when board contains a mark" do
+      context "when not all cells are empty" do
         it "is false" do
           board.set_cell(1, 'X')
           expect(board.empty?).to eq false
@@ -91,33 +107,39 @@ module TicTacToe
     end
 
     describe "#tie_game?" do
-      context "when board is full without winner" do
+      context "when tied" do
         it "is true" do
           expect(tie_board.tie_game?).to eq true
         end
       end
 
-      context "when same mark appears in 3 consecutive cells" do
+      context "when won" do
         it "is false" do
           expect(won_board.tie_game?).to eq false
+        end
+      end
+
+      context "when all cells are empty" do
+        it "is false" do
+          expect(board.tie_game?).to eq false
         end
       end
     end
 
     describe "#over?" do
-      context "when board is full without winner" do
+      context "when tied" do
         it "is true" do
           expect(tie_board.over?).to eq true
         end
       end
 
-      context "when same mark appears in 3 consecutive cells" do
+      context "when won" do
         it "is true" do
           expect(won_board.over?). to eq true
         end
       end
 
-      context "when board is empty" do
+      context "when all cells are empty" do
         it "is false" do
           expect(board.over?).to eq false
         end
