cb63f4ae0dd8216e30cb70c3e675e55396f727af
Lisa Hamm <lisalhamm@gmail.com>
2014-08-24 21:26:25 -0500

    Removed spaces from HumanPlayer test

--- spec/human_player_spec.rb
+++ spec/human_player_spec.rb
@@ -5,12 +5,12 @@ module TicTacToe
     let(:human_player) {HumanPlayer.new('X')}
     let(:board) {Board.new}
 
-      describe "#take_turn" do
-        it "places its mark in selected cell" do
-          allow(human_player).to receive(:get_user_input) { "1" }
-          human_player.take_turn(board)
-          expect(board.get_cell(1).value).to eq "X"
-        end
+    describe "#take_turn" do
+      it "places its mark in selected cell" do
+        allow(human_player).to receive(:get_user_input) {"1"}
+        human_player.take_turn(board)
+        expect(board.get_cell(1).value).to eq "X"
       end
+    end
   end
 end
