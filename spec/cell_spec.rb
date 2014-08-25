dc9e8241071e0ac6c1b363ee3da8cab0c270a94b
Lisa Hamm <lisalhamm@gmail.com>
2014-08-24 20:11:22 -0500

    Added tests for the cell class

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,6 +3,18 @@ require "spec_helper"
 module TicTacToe
   describe Cell do
     let(:cell) {Cell.new}
+    let(:marked_cell) {Cell.new('X')}
+
+
+    describe "#value" do
+      it "is the value" do
+        expect(marked_cell.value).to eq 'X'
+      end
+
+      it "sets the value" do
+        expect(cell.value = 'X').to eq 'X'
+      end
+    end
 
     describe "#empty?" do
       context "when its value is unassigned" do
@@ -13,8 +25,21 @@ module TicTacToe
 
       context "when its value is assigned" do
         it "is false" do
-          cell.value = 'X'
-          expect(cell.empty?).to eq false
+          expect(marked_cell.empty?).to eq false
+        end
+      end
+    end
+
+    describe "#to_s" do
+      context "when empty" do
+        it "is a string containing a space" do
+          expect(cell.to_s).to eq ' '
+        end
+      end
+
+      context "when not empty" do
+        it "is its value" do
+          expect(marked_cell.to_s).to eq 'X'
         end
       end
     end
