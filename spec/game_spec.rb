bfe8f3774b6b4de10ad6415ebbade7e26341d19f
Lisa Hamm <lisalhamm@gmail.com>
2014-09-06 11:58:45 -0500

    Removed contexts from game tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -33,82 +33,58 @@ module TicTacToe
     end
 
     describe "#in_progress?" do
-      context "when in play" do
-        it "is true" do
-          expect(game_in_play.in_progress?).to eq true
-        end
+      it "is true when in play" do
+        expect(game_in_play.in_progress?).to eq true
       end
 
-      context "when won" do
-        it "is false" do
-          expect(won_game.in_progress?).to eq false
-        end
+      it "is false when won" do
+        expect(won_game.in_progress?).to eq false
       end
 
-      context "when tied" do
-        it "is false" do
-          expect(tie_game.in_progress?).to eq false
-        end
+      it "is false when tied" do
+        expect(tie_game.in_progress?).to eq false
       end
     end
 
     describe "#winner?" do
-      context "when in play" do
-        it "is false" do
-          expect(game_in_play.winner?).to eq false
-        end
+      it "is false when in play" do
+        expect(game_in_play.winner?).to eq false
       end
 
-      context "when won" do
-        it "is true" do
-          expect(won_game.winner?).to eq true
-        end
+      it "is true when won" do
+        expect(won_game.winner?).to eq true
       end
 
-      context "when tied" do
-        it "is false" do
-          expect(tie_game.winner?).to eq false
-        end
+      it "is false when tied" do
+        expect(tie_game.winner?).to eq false
       end
     end
 
     describe "#get_winning_player" do
-      context "when won" do
-        it "is winner's mark" do
-          expect(won_game.get_winning_player).to eq player1
-        end
+      it "is winner's mark when won" do
+        expect(won_game.get_winning_player).to eq player1
       end
 
-      context "when tied" do
-        it "is nil" do
-          expect(tie_game.get_winning_player).to eq nil
-        end
+      it "is nil when tied" do
+        expect(tie_game.get_winning_player).to eq nil
       end
 
-      context "when in play" do
-        it "is nil" do
-          expect(game_in_play.get_winning_player).to eq nil
-        end
+      it "is nil when in play" do
+        expect(game_in_play.get_winning_player).to eq nil
       end
     end
 
     describe "#tie?" do
-      context "when in play" do
-        it "is false" do
-          expect(game_in_play.tie?).to eq false
-        end
+      it "is false when in play" do
+        expect(game_in_play.tie?).to eq false
       end
 
-      context "when tied" do
-        it "is true" do
-          expect(tie_game.tie?).to eq true
-        end
+      it "is true when tied" do
+        expect(tie_game.tie?).to eq true
       end
 
-      context "when won" do
-        it "is false" do
-          expect(won_game.tie?).to eq false
-        end
+      it "is false when won" do
+        expect(won_game.tie?).to eq false
       end
     end
   end
